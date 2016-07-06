297ed6b78f63c1db3e03f38ec452c2d00e2480e1
Sid <sidp@knights.ucf.edu>
2016-07-06 19:59:15 -0400

    Updated prod. settings environment variables.

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,9 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-API_KEY = os.environ['API_KEY']
-#DB_PASSWORD = os.environ['DB_PASSWORD']
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+API_KEY = os.environ.get('API_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
